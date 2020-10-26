182710d0d3c84224d6c1e175408744423f55b82f
TakumaOgawa <syuu.2531@gmail.com>
2020-10-26 19:02:59 +0900

    Initial Commit

---
+++ SwiftUI_Realm_1To1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Realm_1To1
+//
+//  Created by 小川卓馬 on 2020/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
